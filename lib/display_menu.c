466ddca31bb4737a7cd5516f703e0d419106cbc4
jiwoo <jiwoo0505@gmail.com>
2017-11-11 09:12:41 -0800

    modified display_menu.c

--- lib/display_menu.c
+++ lib/display_menu.c
@@ -8,18 +8,18 @@ int display_menu(void)
 	while(1)
 	{
 		system("clear");
-		printf("\n\n\t\t\t\t   오 목 게 임    ");
-		printf("\n\n\t\t\t☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★");
-		printf("\n\n\t\t\t\t   게 임 메 뉴\t\n");
-		printf("\n\t\t\t☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★");
-		printf("\n\t\t\t   =\t1) 게임 설명\t   =");
-		printf("\n\t\t\t   =\t2) 1:1 게임\t   =");
-		printf("\n\t\t\t   =\t3) com 초급\t   =");
-		printf("\n\t\t\t   =\t4) com 중급\t   =");
-		printf("\n\t\t\t   =\t5) com 고급\t   =");
-		printf("\n\t\t\t   =\t6) 종료\t\t   =");
-		printf("\n\t\t\t☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★");
-		printf("\n\t\t\t\t\t 선택 : ");
+		printf("\n\n\n\t\t\t\t          오  목  게  임    ");
+		printf("\n\n\t\t\t☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★");
+		printf("\n\n\t\t\t\t          게  임  메  뉴\t");
+		printf("\n\n\t\t\t☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★");
+		printf("\n\n\t\t\t         =\t  (1) 게임 설명       =");
+		printf("\n\n\t\t\t         =\t  (2) 1:1 게임\t      =");
+		printf("\n\n\t\t\t         =\t  (3) com 초급\t      =");
+		printf("\n\n\t\t\t         =\t  (4) com 중급\t      =");
+		printf("\n\n\t\t\t         =\t  (5) com 고급\t      =");
+		printf("\n\n\t\t\t         =\t  (6) 종료\t      =");
+		printf("\n\n\t\t\t☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★ ☆ ★");
+		printf("\n\n\t\t\t\t\t    선택 : ");
 		scanf("%d",&menu);
 		if(menu < 1 || menu > 6)
 		{
