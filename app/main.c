414d122c9c304f82b0d9c33cecbd052586feb82b
jiwoo <jiwoo0505@gmail.com>
2017-11-11 21:24:29 -0800

    add person mode

--- app/main.c
+++ app/main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h> //exit function
-//#include <Windows.h>
-//#include <conio.h>
-#include "map.h"
-#include "display_menu.h"
 
 void map();
 int display_menu();
 int game_manual();
 void gotoxy(int x,int y);
+void game_person();
+
+//int map_info[20][20] = {0};
 int x=10; int y=10;
 
 int main(void)
@@ -27,21 +26,25 @@ int main(void)
 		{
 			map();
 			gotoxy(x,y);
+			game_person();
 			sleep(100000);
 		}
 		else if(menu == 3) //사람 대 컴퓨터 초급모드
 		{
 			map();
+			gotoxy(x,y);
 			return 0;
 		}
 		else if(menu == 4) //사람 대 컴퓨터 중급모드
 		{
 			map();
+			gotoxy(x,y);
 			return 0;
 		}
 		else if(menu == 5) //사람 대 컴퓨터 고급모드
 		{
 			map();
+			gotoxy(x,y);
 			return 0;
 		}
 		else if(menu == 6)
