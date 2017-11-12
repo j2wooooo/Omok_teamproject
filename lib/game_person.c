dc5f180a08c85c83db9324c526e5e21a746f8bd8
jiwoo <jiwoo0505@gmail.com>
2017-11-12 00:25:56 -0800

    modified limit map

--- lib/game_person.c
+++ lib/game_person.c
@@ -3,10 +3,11 @@
 #include "getch.h"
 
 int getch();
-int map_info[20][20];
+extern int map_info[20][40];
 void gotoxy(int x, int y);
-int m=10; int n=10;
+//int m=10; int n=10;
 int color = 2;
+extern int x; int y;
 
 void game_person()
 {
@@ -16,41 +17,50 @@ void game_person()
 		dir = getch();
 		switch (dir) {
 		case 'w':
-			if (m - 1 != 0)
-				n -= 1;
+			//if (m - 1 != 0)
+			//	n -= 1;
+			if(x-1 > 1)
+				x-=1;
 			break;
 		case 'a':
-			if (n - 2 != 0)
-				m -= 2;
+			//if (n - 2 != 0)
+			//	m -= 2;
+			if(y-2 > 0)
+				y -= 2;
 			break;
 		case 's':
-			if (m + 1 != 20)
-				n += 1;
+			//if (m + 1 != 20)
+			//	n += 1;
+			if(x+1 < 20)
+				x += 1;
 			break;
 		case 'd':
-			if (n + 2 != 20)
-				m += 2;
+			//if (n + 2 != 20)
+			//	m += 2;
+			if(y+2 < 40)
+				y += 2;
 			break;
 		case ' ':
-			if (map_info[m][n] == 0)
+			if (map_info[x][y] == 0)
 			{
 				if (color == 1) {
 					printf("○");
-					map_info[m][n] = 1;
+					map_info[x][y] = 1;
 				//	a[x][y] = 1; // color
 					color = 2;
 				}
 				else if (color == 2) {
 					printf("●");
-					map_info[m][n] = 1;
+					map_info[x][y] = 1;
 				//	a[x][y] = 2; // color
 					color = 1;
 				}
 			}
 		}
-		if (map_info[m][n] != 1)
+		if (map_info[x][y] != 1)
 		{
-			gotoxy(m,n);
+			gotoxy(x,y);
+			//printf("%d %d\n", x,y);
 		}
 	}
 }
