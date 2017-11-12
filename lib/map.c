108b4adeac9b7dc6e43b91c9b821d492c4c2559a
jiwoo <jiwoo0505@gmail.com>
2017-11-11 18:18:21 -0800

    init map info

--- lib/map.c
+++ lib/map.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "map.h"
 
+int map_info[20][20] = {0}; // 돌을 놓을 수 있는 위치를 0으로 초기화 해 놓음
+
 void map()
 {
 	system("clear");
@@ -10,22 +12,30 @@ void map()
 	{
 		for(int j = 0; j < 20; j++)
 		{
-			if(i == 0 && j == 0) // drawing start
-				printf("┌─");
-			else if (i == 0 && j == 19)
-				printf("┐");
-			else if (i == 19 && j == 0)
-				printf("└─");
-			else if (i == 19 && j == 19)
-				printf("┘");
-			else if (i == 0)
-				printf("┬─");
-			else if (j == 0)
-				printf("├ ");
-			else if (j == 19)
-				printf("┤");
-			else if (i == 19)
-				printf("┴─");
+			if(i == 0 && j == 0){ // drawing start
+				map_info[i][j] = 1;
+				printf("┌─");}
+			else if (i == 0 && j == 19){
+				map_info[i][j] = 1;
+				printf("┐");}
+			else if (i == 19 && j == 0){
+				map_info[i][j] = 1;
+				printf("└─");}
+			else if (i == 19 && j == 19){
+				map_info[i][j] = 1;
+				printf("┘");}
+			else if (i == 0){
+				map_info[i][j] = 1;
+				printf("┬─");}
+			else if (j == 0){
+				map_info[i][j] = 1;
+				printf("├ ");}
+			else if (j == 19){
+				map_info[i][j] = 1;
+				printf("┤");}
+			else if (i == 19){
+				map_info[i][j] = 1;
+				printf("┴─");}
 			else
 				printf("．");
 		}
