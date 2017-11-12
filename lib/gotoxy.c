b3edb44438e5e22b2d0c99cde83c325533d49a45
jiwoo <jiwoo0505@gmail.com>
2017-11-12 00:26:29 -0800

    modified gotoxy.c

--- lib/gotoxy.c
+++ lib/gotoxy.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
+extern int x ,y;
 
-void gotoxy(int x, int y)
+void gotoxy(int x,int y)
 {
-	printf("\033[%dd\033[%dG",y,x);
+	printf("\033[%dd\033[%dG",x,y);
 	fflush(stdout);
 }
