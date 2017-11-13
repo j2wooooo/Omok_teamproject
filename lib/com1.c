58f73cebd7e9e0e826085dd053715646f78c567c
Juyoung <leejuy1140@gmail.com>
2017-11-13 05:45:13 -0800

    com1 cursor

--- lib/com1.c
+++ lib/com1.c
@@ -3,12 +3,55 @@
 #include "getch.h"
 #include "gotoxy.h"
 
-int main()
-{
- while(1)
- {
-  map();
- }
+extern int map_info[20][40];
+extern int x; int y;
 
- return 0;
+int color = 2;
+
+void com1()
+{
+
+	char dir;
+
+	while(1){
+	        dir = getch();
+                switch (dir) {
+                case 'w':
+                        if(x-1 > 1)
+                                x-=1;
+                        break;
+                case 'a':
+                        if(y-2 > 0)
+                                y -= 2;
+                        break;
+                case 's':
+                        if(x+1 < 20)
+                                x += 1;
+                        break;
+                case 'd':
+                        if(y+2 < 40)
+                                y += 2;
+                        break;
+                case ' ':
+                        if (map_info[x][y] == 0)
+                        {
+                                if (color == 1) {
+                                        printf("○");
+                                        map_info[x][y] = 1;
+                                //      a[x][y] = 1; // color
+                                        color = 2;
+                                }
+                                else if (color == 2) {
+                                        printf("●");
+                                        map_info[x][y] = 1;
+                                //      a[x][y] = 2; // color
+                                }
+                        }
+                }
+                if (map_info[x][y] != 1)
+                {
+                        gotoxy(x,y);
+                        //printf("%d %d\n", x,y);
+                }
+        }
 }
