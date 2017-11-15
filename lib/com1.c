b0fdbd4a5e865948abb404a52964836d63023f70
jiwoo <jiwoo0505@gmail.com>
2017-11-15 07:13:50 -0800

    make new file map_modify.c

--- lib/com1.c
+++ lib/com1.c
@@ -17,48 +17,7 @@ void com1()
 	char dir;
 
 	while(1){
-
-	        dir = getch();
-                switch (dir) {
-                case 'w':
-                        if(x-1 > 1)
-                                x-=1;
-                        break;
-                case 'a':
-                        if(y-2 > 1)
-                                y -= 2;
-                        break;
-                case 's':
-                        if(x+1 < 20)
-                                x += 1;
-                        break;
-                case 'd':
-                        if(y+2 < 40)
-                                y += 2;
-                        break;
-                case ' ':
-                        //if (map_info[x][y] == 0)
-                       //{
-                                if (color == 1) {
-                                        printf("○");
-                                //        map_info[x][y] = 1;
-                                //      a[x][y] = 1; // color
-                                        color = 2;
-                                }
-                                else if (color == 2) {
-                                        printf("●");
-                                //        map_info[x][y] = 1;
-                                //      a[x][y] = 2; // color
-                                }
-                       //}
-                }
-                //if (map_info[x][y] != 1)
-                //{
-                        gotoxy(x,y);
-                        //printf("%d %d\n", x,y);
-                //}
-
-	        if(flag == 0){
+		if(flag == 0){
 			dir = getch();
                 	switch (dir) {
                 	case 'w':
@@ -66,7 +25,7 @@ void com1()
                                 	x-=1;
                         	break;
                 	case 'a':
-                        	if(y-2 > 0)
+                        	if(y-2 > 1)
                                 	y -= 2;
                         	break;
                 	case 's':
--- lib/game_manual.c
+++ lib/game_manual.c
@@ -11,8 +11,8 @@ int game_manual(void)
 	printf("\n\n\n\t\t\t            게임설명서");
 	printf("\n\n\n\t              자신의 돌이 5개 연속으로 놓아지면 승리!");
 	printf("\n\n\n\t\t\t             1대1모드");
-	printf("\n\n      [ 흑돌 ○  ] 위 : W | 아래 : S | 왼쪽 : A | 오른쪽 : D | 돌 놓기 : space");
-	printf("\n\n      [ 백돌 ●  ] 위 : ↑ | 아래 : ↓ | 왼쪽 : ← | 오른쪽 : → | 돌 놓기 : space");
+	printf("\n\n      [ 1 player ]  위 : W | 아래 : S | 왼쪽 : A | 오른쪽 : D | 돌 놓기 : space");
+	printf("\n\n      [ 2 player ] 위 : ↑ | 아래 : ↓ | 왼쪽 : ← | 오른쪽 : → | 돌 놓기 : space");
 	printf("\n\n\n\n\t\t\t            컴퓨터모드");
 	printf("\n\n            위 : ↑ | 아래 : ↓ | 왼쪽 : ← | 오른쪽 : → | 돌 놓기 : space");
 
