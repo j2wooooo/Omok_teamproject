4a79b7a7ec000aa57a5dcf0fdf0011147eab1f5c
Juyoung <leejuy1140@gmail.com>
2017-11-23 03:20:43 -0800

    1st test

--- lib/com1.c
+++ lib/com1.c
@@ -3,12 +3,18 @@
 #include "getch.h"
 #include "gotoxy.h"
 
+#define USR_UP 65 // 방향키 up
+#define USR_DOWN 66 // 방향키 down
+#define USR_RIGHT 67 // 방향키 right
+#define USR_LEFT 68 // 방향키 left
+#define PUT_STONE 32 // 돌 놓기
+
 #define BLACK 1
 #define WHITE 2
 
 extern int map_info[20][20];
-extern int x; int y;
 
+int x; int y;
 int black = 0; // num of black stones
 
 // 행과 열의 흑돌의 개수 카운트
@@ -17,7 +23,7 @@ void checkblack(int x, int y)
 	int row[20]; // 행과 열의 흑돌 개수를 카운트하여 배열에 저장
 	int column[20];
 	int x1 = x; int x2 = y; // tmp x, y
-	
+
 	for(int x1 = 0; x1 < 20; x1++){ // check column
 		for(int y1 = 0; y1 < 20; y1++){
 			if(map_info[x1][y1] == 1){
@@ -36,6 +42,13 @@ void checkblack(int x, int y)
                 black = 0;
 		}
         }
+
+
+	for(int i = 0; i < 20; i++)
+		printf("%d ", row[i]);
+	printf(\n);
+	for(int i = 0; i < 20; i++) 
+                printf("%d ", column[i]);
 }
 
 void com1()
@@ -47,33 +60,32 @@ void com1()
 		if(flag == 0){
 			dir = getch();
                 	switch (dir) {
-                	case 'w':
-                        	if(x-1 > 1)
-                                	x-=1;
-                        	break;
-                	case 'a':
-                        	if(y-2 > 1)
-                                	y -= 2;
-                        	break;
-                	case 's':
-                        	if(x+1 < 20)
-                                	x += 1;
-                        	break;
-                	case 'd':
-                        	if(y+2 < 40)
-                                	y += 2;
-                        	break;
-                	case ' ':
-                       		if (map_info[x][y] == 0)
-                        	{ // if empty location
+
+                	case USR_UP:
+                        	if(x-1 > 1) x-=1; break;
+
+                	case USR_LEFT:
+                        	if(y-2 > 1) y -= 2; break;
+
+                	case USR_DOWN:
+                        	if(x+1 < 20) x += 1; break;
+
+                	case USR_RIGHT:
+                        	if(y+2 < 40) y += 2; break;
+
+                	case PUT_STONE:
+                       		if (map_info[x][y] == 0) // if empty location
+                        	{
 					map_info[x][y] = BLACK;
                                 	printf("●");
-					flag = 1;
+					flag = 1; // com turn
                         	}
                 	}
-                        gotoxy(x,y);
+                        //gotoxy(x,y);
+
 		}
-		else{ // com mode
+		else // com mode
+		{ 
 			checkblack(x,y);
 			flag = 0; // user turn
 		}
