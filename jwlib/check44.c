d92baa7b58b5c06903ae0ea938ac6f8a4d3a1930
jiwoo <jiwoo0505@gmail.com>
2017-12-05 01:31:57 -0800

    make check44 algorithm

---
+++ jwlib/check44.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+struct map_info
+{
+	int put;
+	int color;
+	int dfvalue;
+	int atvalue;
+};
+
+extern struct map_info mi[20][20];
+
+int check44(int x1, int y1)
+{
+	int check = 0;
+
+	// check row
+
+	// check col
+
+	// check diag
+	return check;
+}
