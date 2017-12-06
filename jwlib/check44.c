dc899d761f542c20036ddf474d2a44a79b9606b2
jiwoo <jiwoo0505@gmail.com>
2017-12-05 19:37:40 -0800

    updated check44 algorithm

--- jwlib/check44.c
+++ jwlib/check44.c
@@ -11,12 +11,77 @@ extern struct map_info mi[20][20];
 
 int check44(int x1, int y1)
 {
-	int check = 0;
+	int count = 0;
 
 	// check row
+	if ((mi[x1][y1 / 2 - 4].color == 0 && mi[x1][y1 / 2 - 3].color == 1 && mi[x1][y1 / 2 - 2].color == 1 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 + 1].color == 0) ||
+		(mi[x1][y1 / 2 + 4].color == 0 && mi[x1][y1 / 2 + 3].color == 1 && mi[x1][y1 / 2 + 2].color == 1 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 - 1].color == 0) ||
+		(mi[x1][y1 / 2 - 5].color == 0 && mi[x1][y1 / 2 - 4].color == 1 && mi[x1][y1 / 2 - 3].color == 1 && mi[x1][y1 / 2 - 2].color == 1 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 + 1].color == 0) ||
+		(mi[x1][y1 / 2 + 5].color == 0 && mi[x1][y1 / 2 + 4].color == 1 && mi[x1][y1 / 2 + 3].color == 0 && mi[x1][y1 / 2 + 2].color == 1 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 - 1].color == 0) ||
+		(mi[x1][y1 / 2 - 5].color == 0 && mi[x1][y1 / 2 - 4].color == 1 && mi[x1][y1 / 2 - 3].color == 1 && mi[x1][y1 / 2 - 2].color == 0 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 + 1].color == 0) ||
+		(mi[x1][y1 / 2 + 5].color == 0 && mi[x1][y1 / 2 + 4].color == 1 && mi[x1][y1 / 2 + 3].color == 1 && mi[x1][y1 / 2 + 2].color == 0 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 - 1].color == 0) ||
+		(mi[x1][y1 / 2 - 5].color == 0 && mi[x1][y1 / 2 - 4].color == 1 && mi[x1][y1 / 2 - 3].color == 1 && mi[x1][y1 / 2 - 2].color == 1 && mi[x1][y1 / 2 - 1].color == 0 && mi[x1][y1 / 2 + 1].color == 0) ||
+		(mi[x1][y1 / 2 + 5].color == 0 && mi[x1][y1 / 2 + 4].color == 1 && mi[x1][y1 / 2 + 3].color == 1 && mi[x1][y1 / 2 + 2].color == 1 && mi[x1][y1 / 2 + 1].color == 0 && mi[x1][y1 / 2 - 1].color == 0) ||
+		(mi[x1][y1 / 2 - 3].color == 0 && mi[x1][y1 / 2 - 2].color == 1 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 + 2].color == 0) ||
+		(mi[x1][y1 / 2 + 3].color == 0 && mi[x1][y1 / 2 + 2].color == 1 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 - 2].color == 0) ||
+		(mi[x1][y1 / 2 - 4].color == 0 && mi[x1][y1 / 2 - 3].color == 1 && mi[x1][y1 / 2 - 2].color == 1 && mi[x1][y1 / 2 - 1].color == 0 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 + 2].color == 0) ||
+		(mi[x1][y1 / 2 + 4].color == 0 && mi[x1][y1 / 2 + 3].color == 1 && mi[x1][y1 / 2 + 2].color == 1 && mi[x1][y1 / 2 + 1].color == 0 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 - 2].color == 0) ||
+		(mi[x1][y1 / 2 - 4].color == 0 && mi[x1][y1 / 2 - 3].color == 1 && mi[x1][y1 / 2 - 2].color == 0 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 + 2].color == 0) ||
+		(mi[x1][y1 / 2 + 4].color == 0 && mi[x1][y1 / 2 + 3].color == 1 && mi[x1][y1 / 2 + 2].color == 0 && mi[x1][y1 / 2 + 1].color == 1 && mi[x1][y1 / 2 - 1].color == 1 && mi[x1][y1 / 2 - 2].color == 0))
+		count++;
 
 	// check col
+		if ((mi[x1 - 4][y1 / 2].color == 0 && mi[x1 - 3][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 0) ||
+		(mi[x1 + 4][y1 / 2].color == 0 && mi[x1 + 3][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 0) ||
+		(mi[x1 - 5][y1 / 2].color == 0 && mi[x1 - 4][y1 / 2].color == 1 && mi[x1 - 3][y1 / 2].color == 0 && mi[x1 - 2][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 0) ||
+		(mi[x1 + 5][y1 / 2].color == 0 && mi[x1 + 4][y1 / 2].color == 1 && mi[x1 + 3][y1 / 2].color == 0 && mi[x1 + 2][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 0) ||
+		(mi[x1 - 5][y1 / 2].color == 0 && mi[x1 - 4][y1 / 2].color == 1 && mi[x1 - 3][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 0 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 0) ||
+		(mi[x1 + 5][y1 / 2].color == 0 && mi[x1 + 4][y1 / 2].color == 1 && mi[x1 + 3][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 0 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 0) ||
+		(mi[x1 - 5][y1 / 2].color == 0 && mi[x1 - 4][y1 / 2].color == 1 && mi[x1 - 3][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 0 && mi[x1 + 1][y1 / 2].color == 0) ||
+		(mi[x1 + 5][y1 / 2].color == 0 && mi[x1 + 4][y1 / 2].color == 1 && mi[x1 + 3][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 0 && mi[x1 - 1][y1 / 2].color == 0) ||
+		(mi[x1 - 3][y1 / 2].color == 0 && mi[x1 - 2][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 0) ||
+		(mi[x1 + 3][y1 / 2].color == 0 && mi[x1 + 2][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 0) ||
+		(mi[x1 - 4][y1 / 2].color == 0 && mi[x1 - 3][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 0 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 0) ||
+		(mi[x1 + 4][y1 / 2].color == 0 && mi[x1 + 3][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 0 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 0) ||
+		(mi[x1 - 4][y1 / 2].color == 0 && mi[x1 - 3][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 0 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 0) ||
+		(mi[x1 + 4][y1 / 2].color == 0 && mi[x1 + 3][y1 / 2].color == 1 && mi[x1 + 2][y1 / 2].color == 0 && mi[x1 + 1][y1 / 2].color == 1 && mi[x1 - 1][y1 / 2].color == 1 && mi[x1 - 2][y1 / 2].color == 0))
+		count++;
 
 	// check diag
-	return check;
+
+	// 대각선체크. 
+	if ((mi[x1 - 4][y1 / 2 - 4].color == 0 && mi[x1 - 3][y1 / 2 - 3].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 + 4][y1 / 2 + 4].color == 0 && mi[x1 + 3][y1 / 2 + 3].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 - 5][y1 / 2 - 5].color == 0 && mi[x1 - 4][y1 / 2 - 4].color == 1 && mi[x1 - 3][y1 / 2 - 3].color == 0 && mi[x1 - 2][y1 / 2 - 2].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 + 5][y1 / 2 + 5].color == 0 && mi[x1 + 4][y1 / 2 + 4].color == 1 && mi[x1 + 3][y1 / 2 + 3].color == 0 && mi[x1 + 2][y1 / 2 + 2].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 - 5][y1 / 2 - 5].color == 0 && mi[x1 - 4][y1 / 2 - 4].color == 1 && mi[x1 - 3][y1 / 2 - 3].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 0 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 + 5][y1 / 2 + 5].color == 0 && mi[x1 + 4][y1 / 2 + 4].color == 1 && mi[x1 + 3][y1 / 2 + 3].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 0 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 - 5][y1 / 2 - 5].color == 0 && mi[x1 - 4][y1 / 2 - 4].color == 1 && mi[x1 - 3][y1 / 2 - 3].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 0 && mi[x1 + 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 + 5][y1 / 2 + 5].color == 0 && mi[x1 + 4][y1 / 2 + 4].color == 1 && mi[x1 + 3][y1 / 2 + 3].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 0 && mi[x1 - 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 - 3][y1 / 2 - 3].color == 0 && mi[x1 - 2][y1 / 2 - 2].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 0) ||
+		(mi[x1 + 3][y1 / 2 + 3].color == 0 && mi[x1 + 2][y1 / 2 + 2].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 0) ||
+		(mi[x1 - 4][y1 / 2 - 4].color == 0 && mi[x1 - 3][y1 / 2 - 3].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 0 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 0) ||
+		(mi[x1 + 4][y1 / 2 + 4].color == 0 && mi[x1 + 3][y1 / 2 + 3].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 0 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 0) ||
+		(mi[x1 - 4][y1 / 2 - 4].color == 0 && mi[x1 - 3][y1 / 2 - 3].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 0 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 0) ||
+		(mi[x1 + 4][y1 / 2 + 4].color == 0 && mi[x1 + 3][y1 / 2 + 3].color == 1 && mi[x1 + 2][y1 / 2 + 2].color == 0 && mi[x1 + 1][y1 / 2 + 1].color == 1 && mi[x1 - 1][y1 / 2 - 1].color == 1 && mi[x1 - 2][y1 / 2 - 2].color == 0))
+		count++;
+	// 반대 대각선체크. 
+	if ((mi[x1 - 4][y1 / 2 + 4].color == 0 && mi[x1 - 3][y1 / 2 + 3].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 + 4][y1 / 2 - 4].color == 0 && mi[x1 + 3][y1 / 2 - 3].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 - 5][y1 / 2 + 5].color == 0 && mi[x1 - 4][y1 / 2 + 4].color == 1 && mi[x1 - 3][y1 / 2 + 3].color == 0 && mi[x1 - 2][y1 / 2 + 2].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 + 5][y1 / 2 - 5].color == 0 && mi[x1 + 4][y1 / 2 - 4].color == 1 && mi[x1 + 3][y1 / 2 - 3].color == 0 && mi[x1 + 2][y1 / 2 - 2].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 - 5][y1 / 2 + 5].color == 0 && mi[x1 - 4][y1 / 2 + 4].color == 1 && mi[x1 - 3][y1 / 2 + 3].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 0 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 + 5][y1 / 2 - 5].color == 0 && mi[x1 + 4][y1 / 2 - 4].color == 1 && mi[x1 + 3][y1 / 2 - 3].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 0 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 - 5][y1 / 2 - 5].color == 0 && mi[x1 - 4][y1 / 2 + 4].color == 1 && mi[x1 - 3][y1 / 2 + 3].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 0 && mi[x1 + 1][y1 / 2 - 1].color == 0) ||
+		(mi[x1 + 5][y1 / 2 + 5].color == 0 && mi[x1 + 4][y1 / 2 - 4].color == 1 && mi[x1 + 3][y1 / 2 - 3].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 0 && mi[x1 - 1][y1 / 2 + 1].color == 0) ||
+		(mi[x1 - 3][y1 / 2 + 3].color == 0 && mi[x1 - 2][y1 / 2 + 2].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 0) ||
+		(mi[x1 + 3][y1 / 2 - 3].color == 0 && mi[x1 + 2][y1 / 2 - 2].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 0) ||
+		(mi[x1 - 4][y1 / 2 + 4].color == 0 && mi[x1 - 3][y1 / 2 + 3].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 0 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 0) ||
+		(mi[x1 + 4][y1 / 2 - 4].color == 0 && mi[x1 + 3][y1 / 2 - 3].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 0 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 0) ||
+		(mi[x1 - 4][y1 / 2 + 4].color == 0 && mi[x1 - 3][y1 / 2 + 3].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 0 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 0) ||
+		(mi[x1 + 4][y1 / 2 - 4].color == 0 && mi[x1 + 3][y1 / 2 - 3].color == 1 && mi[x1 + 2][y1 / 2 - 2].color == 0 && mi[x1 + 1][y1 / 2 - 1].color == 1 && mi[x1 - 1][y1 / 2 + 1].color == 1 && mi[x1 - 2][y1 / 2 + 2].color == 0))
+		count++;
+
+	if (count > 1) return 1;
+	else return 0;
 }
